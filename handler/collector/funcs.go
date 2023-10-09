6d45f6b8aae0fae9e373196a7ee8011326a734aa
chrisarends09 <chrisarends09@gmail.com>
2023-10-08 21:22:25 -0700

    Change go time

--- handler/collector/funcs.go
+++ handler/collector/funcs.go
@@ -8,15 +8,17 @@ import (
 func formatDate(date time.Time, timezone string) string {
 	loc, _ := time.LoadLocation(timezone)
 	date = date.In(loc)
-	return date.Format("02/01/2006")
+	return date.Format("01/02/2006")
 }
 
 func formatHour(date time.Time, timezone string) string {
 	loc, _ := time.LoadLocation(timezone)
 	date = date.In(loc)
-	return date.Format("15:04")
+	// return date.Format("15:04")  this is 24 Hour time
+	return date.Format("03:04")
 }
 
 func formatTime(date time.Time) string {
-	return date.Format(time.RFC3339)
+	//return date.Format(time.RFC3339)
+	return date.Format(time.RubyDate)
 }
